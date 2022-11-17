8a23e427e0d99d1cc5d278d5241f9d4d54480855
PDMA1979 <pedrodomingomurillo1979@gmail.com>
2022-11-17 02:33:42 -0400

    Actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Mi Casa'),
     );
   }
 }
@@ -33,40 +33,221 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyHomePage> {
+  DatabaseReference foco1 = FirebaseDatabase.instance.ref("/casa/foco");
+  DatabaseReference foco2 = FirebaseDatabase.instance.ref("/casa/foco2");
+  DatabaseReference foco3 = FirebaseDatabase.instance.ref("/casa/foco3");
+  DatabaseReference foco4 = FirebaseDatabase.instance.ref("/casa/foco4");
+  DatabaseReference puerta = FirebaseDatabase.instance.ref("/casa/puerta");
+  DatabaseReference refCasa = FirebaseDatabase.instance.ref("/casa");
+
+  bool _fc1 = false;
+  bool _fc2 = false;
+  bool _fc3 = false;
+  bool _fc4 = false;
+  bool _cp = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _encenderFoco();
+    _encenderFoco2();
+    _encenderFoco3();
+    _encenderFoco4();
+    _abrirChapa();
+  }
+
+  void _encenderFoco() async {
+    bool estado;
+    await foco1.get().then((value) => {
+          if (value.value == true)
+            {
+              estado = false,
+              setState(() {
+                _fc1 = false;
+              })
+            }
+          else
+            {
+              estado = true,
+              setState(() {
+                _fc1 = true;
+              })
+            },
+          refCasa.update({
+            "foco": estado,
+          }),
+          print("Iniciado 1")
+        });
+  }
+
+  void _encenderFoco2() async {
+    bool estado;
+    await foco2.get().then((value) => {
+          if (value.value == true)
+            {
+              estado = false,
+              setState(() {
+                _fc2 = false;
+              })
+            }
+          else
+            {
+              estado = true,
+              setState(() {
+                _fc2 = true;
+              })
+            },
+          refCasa.update({
+            "foco2": estado,
+          }),
+          print("Iniciado 2")
+        });
+  }
+
+  void _encenderFoco3() async {
+    bool estado;
+    await foco3.get().then((value) => {
+          if (value.value == true)
+            {
+              estado = false,
+              setState(() {
+                _fc3 = false;
+              })
+            }
+          else
+            {
+              estado = true,
+              setState(() {
+                _fc3 = true;
+              })
+            },
+          refCasa.update({
+            "foco3": estado,
+          })
+        });
+  }
+
+  void _encenderFoco4() async {
+    bool estado;
+    await foco4.get().then((value) => {
+          if (value.value == true)
+            {
+              estado = false,
+              setState(() {
+                _fc4 = false;
+              })
+            }
+          else
+            {
+              estado = true,
+              setState(() {
+                _fc4 = true;
+              })
+            },
+          refCasa.update({
+            "foco4": estado,
+          })
+        });
+  }
+
+  void _abrirChapa() async {
+    bool estado;
+    await puerta.get().then((value) => {
+          print(value.value),
+          if (value.value == true)
+            {
+              estado = false,
+              setState(() {
+                _cp = false;
+              })
+            }
+          else
+            {
+              estado = true,
+              setState(() {
+                _cp = true;
+              })
+            },
+          refCasa.update({
+            "puerta": estado,
+          })
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    void _encenderFoco() async {
-      bool estado;
-
-      DatabaseReference ref1 = FirebaseDatabase.instance.ref("/casa/foco");
-      DatabaseReference ref2 = FirebaseDatabase.instance.ref("/casa");
-
-      await ref1.get().then((value) => {
-            print(value.value),
-            if (value.value == true) {estado = false} else {estado = true},
-            ref2.update({
-              "foco": estado,
-            })
-          });
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
           children: <Widget>[
+            const SizedBox(
+              height: 100,
+            ),
+            const Text(
+              'Focos de la casa',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 ElevatedButton(
-                    onPressed: _encenderFoco, child: const Text('Foco 1'))
+                  onPressed: _encenderFoco,
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          _fc1 == true ? Colors.amber : Colors.white,
+                      foregroundColor: Colors.black),
+                  child: const Text('Foco 1'),
+                ),
+                ElevatedButton(
+                    onPressed: _encenderFoco2,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            _fc2 == true ? Colors.amber : Colors.white,
+                        foregroundColor: Colors.black),
+                    child: const Text('Foco 2')),
+                ElevatedButton(
+                    onPressed: _encenderFoco3,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            _fc3 == true ? Colors.amber : Colors.white,
+                        foregroundColor: Colors.black),
+                    child: const Text('Foco 3')),
+                ElevatedButton(
+                    onPressed: _encenderFoco4,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            _fc4 == true ? Colors.amber : Colors.white,
+                        foregroundColor: Colors.black),
+                    child: const Text('Foco 4')),
               ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              'Puerta de la casa',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: _abrirChapa,
+              child: Text(_cp == true ? 'Abierto' : 'Cerrado'),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: _cp == true ? Colors.green : Colors.red,
+                  foregroundColor: Colors.white),
             )
           ],
         ),
